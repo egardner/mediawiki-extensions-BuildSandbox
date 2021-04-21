f36551822226c5a523b3f7798d5322e410c3c814
Eric Gardner <egardner@wikimedia.org>
2021-04-20 20:26:14 -0700

    Set up sandbox special page

---
+++ includes/SpecialBuildSandbox.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MediaWiki\Extension\BuildSandbox;
+
+class SpecialBuildSandbox extends \SpecialPage {
+
+	public function __construct() {
+		parent::__construct( 'BuildSandbox' );
+	}
+
+	/**
+	 * Set up the special page
+	 * @param string $sub
+	 */
+	public function execute( $sub ) {
+		$out = $this->getOutput();
+		$markup = "<div id='sandbox'><p>This message will dissappear once JS initializes</p></div>";
+		$out->setPageTitle( $this->msg( 'buildsandbox-title' ) );
+		$out->addHTML( $markup );
+		$out->addModules( 'ext.buildSandbox.main' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGroupNames() {
+		return 'other';
+	}
+}
